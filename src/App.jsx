3691a876bff70913b9211b7d930a1209cc669314
Deepak Dhakad <deepakdkd1188@gmail.com>
2023-12-11 03:16:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,44 @@ import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([])
-  const addTodo = (todo) =>{
-    setTodos([ {id: Date.now(), ...todo} , ...todos])
+  const addTodo = (todo) => {
+    setTodos([{ id: Date.now(), ...todo }, ...todos])
   }
 
-  const updateTodo = (id , todo) =>{
-    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id)? todo : prevTodo))
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id) ? todo : prevTodo))
   }
 
-  const deleteTodo = (id) =>{
+  const deleteTodo = (id) => {
     setTodos((prev) => prev.filter(todo => todo.id !== id))
   }
 
-  const toggleComplete = (id) =>{
-    setTodos(prev => prev.map(todo => todo.id == id ? {...todo, completed: !todo.completed} : todo))
+  const toggleComplete = (id) => {
+    setTodos(prev => prev.map(todo => todo.id == id ? { ...todo, completed: !todo.completed } : todo))
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     const todos = JSON.parse(localStorage.getItem('todos'))
-    if(todos && todos.length > 0){
+    if (todos && todos.length > 0) {
       setTodos(todos)
     }
-  },[])
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos))
-  },[todos])
+  }, [todos])
 
-  
+
   return (
-    <TodoProvider value={{todos , addTodo , updateTodo , deleteTodo ,toggleComplete}}> 
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <h1 class="title">Todo Space 📝</h1>
       <div className="App">
         <TodoForm />
 
         <div className="todo-items">
           {todos.map((todo) =>
-              (<div key={todo.id}><TodoItem todo={todo}/></div>))}
+            (<div key={todo.id}><TodoItem todo={todo} /></div>))}
+            <h2>“Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.”</h2>
         </div>
       </div>
     </TodoProvider>
